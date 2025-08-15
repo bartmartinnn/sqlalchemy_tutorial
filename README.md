748cf73d96fd05c1ed863e18368e967b58059196
bartmartinnn <bartmartinnn@gmail.com>
2025-08-15 08:34:44 +0200

    finished 2/3 offirst part

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sqlalchemy_tutorial
 Following https://docs.sqlalchemy.org/en/20/tutorial/engine.html
+
+next steps: https://docs.sqlalchemy.org/en/20/tutorial/dbapi_transactions.html#sending-parameters
--- main.py
+++ main.py
@@ -8,3 +8,22 @@ with engine.connect() as conn:
     result = conn.execute(text('select * from some_table'))
     print(result.all())
     conn.commit()  
+
+'begin once'
+with engine.begin() as conn:
+    conn.execute(text('insert into some_table (x, y) values (:x, :y)'),
+                    [{'x': 1, 'y': 2}, {'x': 3, 'y': 4}])
+    result = conn.execute(text('select * from some_table'))
+    print('tuple assignment')
+    for x, y in result:
+        print(f'x: {x}, y: {y}')
+    
+    result = conn.execute(text('select x, y from some_table'))
+    print('attribute access')
+    for row in result:
+        print(f'x: {row.x}, y: {row.y}')
+    
+    result = conn.execute(text('select x, y from some_table'))
+    print('dictionary access')
+    for dict_row in result.mappings():
+        print(f'x: {dict_row['x']}, y: {dict_row['y']}')
