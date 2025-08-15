aaef31247c689daf8a2c7e493d9401027776e1b3
bartmartinnn <bartmartinnn@gmail.com>
2025-08-15 08:18:59 +0200

    in memory  table

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from sqlalchemy import create_engine, text
 engine = create_engine('sqlite+pysqlite:///:memory:', echo=True)
 
 with engine.connect() as conn:
-    result = conn.execute(text('select "hello world"'))
+    conn.execute(text('create table some_table (x int, y int)'))
+    conn.execute(text('insert into some_table (x, y) values (:x, :y)'),
+                    [{'x': 1, 'y': 2}, {'x': 3, 'y': 4}])
+    result = conn.execute(text('select * from some_table'))
     print(result.all())
+    conn.commit()  
